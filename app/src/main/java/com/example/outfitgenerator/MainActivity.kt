0d9d285a03051ac1949dd215693e93f39d314b94
ElliotFackler <99917625+ElliotFackler@users.noreply.github.com>
2022-04-04 22:21:03 -0500

    BOTH FRAGMENTS ARE WORKING AND RUNNING. Let's go

--- app/src/main/java/com/example/outfitgenerator/MainActivity.kt
+++ app/src/main/java/com/example/outfitgenerator/MainActivity.kt
@@ -2,10 +2,11 @@ package com.example.outfitgenerator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
-
-class MainActivity : AppCompatActivity() {
+private const val TAG = "MainActivity"
+class MainActivity : AppCompatActivity(), FirstFragment.Callbacks {
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,10 +16,19 @@ class MainActivity : AppCompatActivity() {
 
 
         val currentFragment = supportFragmentManager.findFragmentById(R.id.fragment_container)
+
         if (currentFragment == null){
             val fragment = FirstFragment()
             supportFragmentManager.beginTransaction().add(R.id.fragment_container, fragment).commit()
         }
 
+
     }
+
+    override fun startPhotoFragment() {
+        val fragment1 = PhotoFragment()
+        supportFragmentManager.beginTransaction().replace(R.id.fragment_container, fragment1).addToBackStack(null).commit()
+
+    }
+
 }
