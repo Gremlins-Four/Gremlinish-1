d9c30e5b8da9403fc4497130ac99f8381da684a3
amyrgilh <amygilhoi@gustavus.edu>
2022-05-10 08:41:54 -0500

    fix outfit viw

--- app/src/main/java/com/example/outfitgenerator/SavedOutfit.kt
+++ app/src/main/java/com/example/outfitgenerator/SavedOutfit.kt
@@ -3,9 +3,9 @@ package com.example.outfitgenerator
 import com.google.firebase.database.Exclude
 import java.util.*
 var savedOutfitList = mutableListOf<SavedOutfit>()
-data class SavedOutfit(var hatImage: String ?= null,
-                       var shirtImage: String ?= null,
-                       var pantsImage: String ?= null,
-                       var shoesImage: String ?= null,
+data class SavedOutfit(var hatImage: Int ?= null,
+                       var shirtImage: Int ?= null,
+                       var pantsImage: Int ?= null,
+                       var shoesImage: Int ?= null,
                        val id: Int? = savedOutfitList.size )
 
